5fe4e99fa79060c349b0acf73e9dee2013e40746
devNagumo <gustavo.santosdev12@gmail.com>
2025-08-18 23:51:33 -0300

    refeito sidebar e home

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(link => {
+  link.addEventListener("click", function () {
+    links.forEach(l => l.classList.remove("active"));
+    
+    this.classList.toggle("active");
+  });
+});
